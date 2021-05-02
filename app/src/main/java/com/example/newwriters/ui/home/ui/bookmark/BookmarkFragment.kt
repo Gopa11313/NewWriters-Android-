2b5f8ee480b60e28661b30f8ab5c6646f986d6db
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-02 18:59:17 +0545

    done with bootom navigation and their fragmments

---
+++ app/src/main/java/com/example/newwriters/ui/home/ui/bookmark/BookmarkFragment.kt
@@ -0,0 +1,30 @@
+package com.example.newwriters.ui.home.ui.bookmark
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.example.newwriters.R
+import com.example.newwriters.ui.home.ui.home.HomeViewModel
+
+class BookmarkFragment : Fragment() {
+
+    private lateinit var viewModel: BookmarkViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        viewModel =
+            ViewModelProvider(this).get(BookmarkViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        viewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+}
