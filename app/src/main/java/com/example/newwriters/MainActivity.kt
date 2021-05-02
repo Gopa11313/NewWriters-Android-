29a05ea86bd63a2bcae2920af76be4575c1c955b
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-02 11:20:16 +0545

    starting to build NewWriters android application

---
+++ app/src/main/java/com/example/newwriters/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.newwriters
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import com.example.newwriters.UI.Login.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var Sologan:TextView
+    private lateinit var fadein:Animation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Sologan=findViewById(R.id.Sologan)
+        fadein=AnimationUtils.loadAnimation(this,R.anim.fadein)
+        Sologan.startAnimation(fadein)
+        Handler(Looper.getMainLooper()).postDelayed({
+    startActivity()
+        },2500)
+
+    }
+    fun startActivity(){
+        startActivity(Intent(this,LoginActivity::class.java))
+        finish()
+    }
+}
