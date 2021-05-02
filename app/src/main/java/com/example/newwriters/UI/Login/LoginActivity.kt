cd516438f96f48a30b118de6ba3c1f3b00f638b5
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-02 13:59:18 +0545

    connect login and sign up

--- app/src/main/java/com/example/newwriters/UI/Login/LoginActivity.kt
+++ app/src/main/java/com/example/newwriters/UI/Login/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.example.newwriters.UI.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import com.example.newwriters.R
+import com.example.newwriters.UI.Signup.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var lg_email:EditText
+    private lateinit var lg_password:EditText
+    private lateinit var lg_showPassword:CheckBox
+    private lateinit var login:Button
+    private lateinit var lg_signup:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        lg_email=findViewById(R.id.lg_email)
+        lg_password=findViewById(R.id.lg_password)
+        lg_showPassword=findViewById(R.id.lg_showPassword)
+        login=findViewById(R.id.login)
+        lg_signup=findViewById(R.id.lg_signup)
+        lg_signup.setOnClickListener(){
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/newwriters/UI/Signup/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newwriters.UI.Signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newwriters.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
