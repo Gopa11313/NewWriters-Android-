75fd1b37d311e4929be8f76d8c58c677b7e60e62
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-02 20:56:35 +0545

    Changes in homeactivity

--- app/src/main/java/com/example/newwriters/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/newwriters/ui/home/HomeActivity.kt
@@ -15,7 +15,6 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
