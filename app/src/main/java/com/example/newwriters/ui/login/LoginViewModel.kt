0dc739b2a1308d0d32d213b840ca0e844d94f256
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-02 14:13:45 +0545

    viewmodel for login activity

---
+++ app/src/main/java/com/example/newwriters/ui/login/LoginViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.newwriters.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel:ViewModel() {
+    val _email=MutableLiveData<String>()
+    val _password=MutableLiveData<String>()
+    val email:LiveData<String>
+        get()=_email
+    val password:LiveData<String>
+        get()=_password
+}
