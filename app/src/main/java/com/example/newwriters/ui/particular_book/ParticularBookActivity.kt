3ced172f63b5b536a9d8147729620564b22cfc2a
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-03 20:27:04 +0545

    particular activity

--- app/src/main/java/com/example/newwriters/ui/particular_book/ParticularBookActivity.kt
+++ app/src/main/java/com/example/newwriters/ui/particular_book/ParticularBookActivity.kt
@@ -2,6 +2,7 @@ package com.example.newwriters.ui.particular_book
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newwriters.R
 
 class ParticularBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
