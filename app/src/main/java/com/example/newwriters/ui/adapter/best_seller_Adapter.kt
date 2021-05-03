78ecfb447fbddb1471a5e02f588624020a533a08
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-03 13:57:45 +0545

    done with best seller recyclar

--- app/src/main/java/com/example/newwriters/ui/adapter/best_seller_Adapter.kt
+++ app/src/main/java/com/example/newwriters/ui/adapter/best_seller_Adapter.kt
@@ -10,7 +10,6 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.newwriters.R
-import com.example.newwriters.databinding.ActivityLoginBinding.inflate
 import com.example.newwriters.ui.model.Best_Seller
 
 class best_seller_Adapter(
