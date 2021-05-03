6e0ab72cedf8243031b0331611423b89f623ca17
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-03 20:27:31 +0545

    some changes in best_seller model

--- app/src/main/java/com/example/newwriters/ui/model/Best_Seller.kt
+++ app/src/main/java/com/example/newwriters/ui/model/Best_Seller.kt
@@ -1,6 +1,29 @@
 package com.example.newwriters.ui.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Best_Seller(
        val image:String?=null
-) {
+):Parcelable {
+       constructor(parcel: Parcel) : this(parcel.readString()) {
+       }
+
+       override fun writeToParcel(parcel: Parcel, flags: Int) {
+              parcel.writeString(image)
+       }
+
+       override fun describeContents(): Int {
+              return 0
+       }
+
+       companion object CREATOR : Parcelable.Creator<Best_Seller> {
+              override fun createFromParcel(parcel: Parcel): Best_Seller {
+                     return Best_Seller(parcel)
+              }
+
+              override fun newArray(size: Int): Array<Best_Seller?> {
+                     return arrayOfNulls(size)
+              }
+       }
 }
