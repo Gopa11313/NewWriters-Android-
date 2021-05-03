0f6251aec2280a9cd23936c4fc62a7db393f9d39
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-03 12:12:09 +0545

    binding homefragment with homefragment.kt

--- app/src/main/java/com/example/newwriters/ui/home/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/newwriters/ui/home/ui/home/HomeFragment.kt
@@ -8,12 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newwriters.R
-//import com.example.newwriters.ui.home.R
 
 class HomeFragment : Fragment() {
-
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var rcy_bst_seller: RecyclerView
+    private lateinit var rcy_top_rated: RecyclerView
+    private lateinit var rcy_new_publised: RecyclerView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -28,5 +30,12 @@ class HomeFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rcy_bst_seller=view.findViewById(R.id.rcy_bst_seller)
+        rcy_top_rated=view.findViewById(R.id.rcy_top_rated)
+        rcy_new_publised=view.findViewById(R.id.rcy_new_publised)
     }
 }
