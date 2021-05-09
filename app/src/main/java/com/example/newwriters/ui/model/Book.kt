c9c4f43637857711ddd4c5cfb51c158fc898c64b
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-09 19:57:45 +0545

    book model for displaying book

---
+++ app/src/main/java/com/example/newwriters/ui/model/Book.kt
@@ -0,0 +1,10 @@
+package com.example.newwriters.ui.model
+
+data class Book(
+        val book_name:String?=null,
+        val cover_page: String?=null,
+        val ratting:Float?=null,
+        val author_name:String?=null
+)
+{
+}
