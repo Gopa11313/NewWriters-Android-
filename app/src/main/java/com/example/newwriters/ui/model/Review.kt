ff85efa02c4b7b72cd908ff034462fbea34da23c
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-09 11:56:02 +0545

    done with review model

---
+++ app/src/main/java/com/example/newwriters/ui/model/Review.kt
@@ -0,0 +1,9 @@
+package com.example.newwriters.ui.model
+
+data class Review(
+        val ratting:Float?=null,
+        val name:String?=null,
+        val date:String?=null,
+        val review:String?=null
+) {
+}
