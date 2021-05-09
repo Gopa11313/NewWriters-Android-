5178711026664fad7cd1f46096b01b845dea12a2
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-09 12:27:32 +0545

    done with bookmakr model

---
+++ app/src/main/java/com/example/newwriters/ui/model/Bookmark.kt
@@ -0,0 +1,12 @@
+package com.example.newwriters.ui.model
+
+import android.widget.ImageView
+
+data class Bookmark(
+        val book_name:String?=null,
+        val cover_page: String?=null,
+        val ratting:Float?=null,
+        val author_name:String?=null,
+)
+{
+}
