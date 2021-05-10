657485694e1e6f5d544bcbf11bd57bdf053a5360
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-10 19:08:34 +0545

    trying to fetch image from internal storage

--- app/src/main/java/com/example/newwriters/ui/admin/home/ui/uploadbooks/UploadBookFragment.kt
+++ app/src/main/java/com/example/newwriters/ui/admin/home/ui/uploadbooks/UploadBookFragment.kt
@@ -1,22 +1,42 @@
 package com.example.newwriters.ui.admin.home.ui.uploadbooks
 
+import android.R.attr
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.newwriters.R
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+
+
 class UploadBookFragment : Fragment() {
 
     private lateinit var uploadBookViewModel: UploadBookViewModel
-
+    private lateinit var CoverPage: ImageView
+    private lateinit var bk_name: TextView
+    private val REQUEST_GALLERY_CODE=0;
+    private val REQUEST_CAMERA_CODE=1;
+    private var imageUrl:String?=null;
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         uploadBookViewModel =
             ViewModelProvider(this).get(UploadBookViewModel::class.java)
@@ -26,5 +46,94 @@ class UploadBookFragment : Fragment() {
 //            textView.text = it
 //        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        CoverPage=view.findViewById(R.id.CoverPage)
+        bk_name=view.findViewById(R.id.bk_name)
+        CoverPage.setOnClickListener(){
+            popup()
+        }
+    }
+    private fun popup(){
+        val popupMenu= PopupMenu(requireContext(), CoverPage)
+        popupMenu.menuInflater.inflate(R.menu.pop_up_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.gallery -> {
+                    openGallery()
+                    true
+                }
+                R.id.camera -> {
+                    openCamera()
+                    true
+                }
+                else -> false
+            }
+
+        })
+        popupMenu.show()
+    }
+    private fun openGallery(){
+        val intent= Intent(Intent.ACTION_PICK)
+        intent.type="image/*"
+        startActivityForResult(intent, REQUEST_GALLERY_CODE)
+    }
+    private fun openCamera(){
+        val cameraIntent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CAMERA_CODE)
+    }
+    private fun bitmapToFile(
+            bitmap: Bitmap,
+            fileNameToSave: String
+    ): File? {
+        var file: File? = null
+        return try {
+            file = File(
+                    requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                            .toString() + File.separator + fileNameToSave
+            )
+            file.createNewFile()
+            //Convert bitmap to byte array
+            val bos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos) // YOU can also save it in JPEG
+            val bitMapData = bos.toByteArray()
+            //write the bytes in file
+            val fos = FileOutputStream(file)
+            fos.write(bitMapData)
+            fos.flush()
+            fos.close()
+            file
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            file // it will return null
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_GALLERY_CODE && data != null) {
+                val selectedImage = data.data
+                val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                val contentResolver = requireActivity().contentResolver
+                val cursor =
+                        contentResolver.query(selectedImage!!, filePathColumn, null, null, null)
+                cursor!!.moveToFirst()
+                val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+                imageUrl = cursor.getString(columnIndex)
+                CoverPage.setImageBitmap(BitmapFactory.decodeFile(imageUrl))
+                cursor.close()
+                bk_name.setText("$imageUrl")
+                Toast.makeText(requireContext(), "this is imgeurl: $imageUrl", Toast.LENGTH_LONG).show()
+                } else if (requestCode == REQUEST_CAMERA_CODE && data != null) {
+                    val imageBitmap = data.extras?.get("data") as Bitmap
+                    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+                    val file = bitmapToFile(imageBitmap, "$timeStamp.jpg")
+                    imageUrl = file!!.absolutePath
+                    CoverPage.setImageBitmap(BitmapFactory.decodeFile(imageUrl))
+                    bk_name.setText("$imageUrl")
+                }
+        }
     }
 }
