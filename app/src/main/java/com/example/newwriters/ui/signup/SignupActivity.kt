1dee39bf10158c7f4883c5ca7b82cc64b7c7e41d
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-11 11:40:10 +0545

    user register successfully!!

--- app/src/main/java/com/example/newwriters/ui/signup/SignupActivity.kt
+++ app/src/main/java/com/example/newwriters/ui/signup/SignupActivity.kt
@@ -6,8 +6,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
 import com.example.newwriters.R
+import com.example.newwriters.repository.UserRepository
 import com.example.newwriters.ui.login.LoginActivity
+import com.example.newwriters.ui.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var sg_name:EditText
@@ -27,6 +35,27 @@ class SignupActivity : AppCompatActivity() {
         sg_signin=findViewById(R.id.sg_signin)
         sg_signin.setOnClickListener(){
             startActivity(Intent(this,LoginActivity::class.java))
+        }
+        signup.setOnClickListener(){
+            userSignUp()
+        }
+    }
+    private fun userSignUp(){
+        val user= User(name = sg_name.text.toString(),email = sg_email.text.toString(),password = sg_password.text.toString())
+        CoroutineScope(Dispatchers.IO).launch {
+            val repository=UserRepository()
+            val response=repository.SignupUSer(user)
+            if(response.success==true){
+                withContext(Main){
+                    Toast.makeText(this@SignupActivity, "User Register Successfully!!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            else{
+                withContext(Main){
+                    Toast.makeText(this@SignupActivity, response.msg, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
