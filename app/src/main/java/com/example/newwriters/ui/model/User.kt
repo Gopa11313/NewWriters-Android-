ab1e60662555a917b2359189bca237ad026d4cd2
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-11 11:28:49 +0545

    done with user model

---
+++ app/src/main/java/com/example/newwriters/ui/model/User.kt
@@ -0,0 +1,10 @@
+package com.example.newwriters.ui.model
+
+data class User(
+    val _id:String?=null,
+    val name:String?=null,
+    val email:String?=null,
+    val password:String?=null,
+    val role:String?=null
+) {
+}
