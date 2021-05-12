7503eec111a092a1a5606e2824a16378299df261
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-12 15:04:56 +0545

    done with book model

--- app/src/main/java/com/example/newwriters/ui/model/Book.kt
+++ app/src/main/java/com/example/newwriters/ui/model/Book.kt
@@ -1,10 +1,16 @@
 package com.example.newwriters.ui.model
 
 data class Book(
+        val _id:String?=null,
         val book_name:String?=null,
         val cover_page: String?=null,
+        val book: String?=null,
         val ratting:Float?=null,
-        val author_name:String?=null
+        val noofRating:Int?=null,
+        val author_name:String?=null,
+        val description:String?=null,
+        val price:String?=null,
+        val offer:String?=null
 )
 {
 }
