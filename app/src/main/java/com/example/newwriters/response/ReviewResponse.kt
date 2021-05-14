7455bfd38d5cb6850b7880f158831b759ecfc220
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-15 00:37:22 +0545

    done with fetching review

--- app/src/main/java/com/example/newwriters/response/ReviewResponse.kt
+++ app/src/main/java/com/example/newwriters/response/ReviewResponse.kt
@@ -1,11 +1,12 @@
 package com.example.newwriters.response
 
 import com.example.newwriters.ui.model.Book
+import com.example.newwriters.ui.model.Review
 
 class ReviewResponse (
     val success:Boolean?=null,
     val msg:String?=null,
-    val data:MutableList<Book>?=null,
+    val data:MutableList<Review>?=null,
     val id:String?=null
 ){
 }
