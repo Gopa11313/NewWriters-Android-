d30e3bc2d3f09b491d812e8ca5e1cad60416b173
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-14 14:47:26 +0545

    repository for review api

---
+++ app/src/main/java/com/example/newwriters/repository/ReviewRepository.kt
@@ -0,0 +1,18 @@
+package com.example.newwriters.repository
+
+import com.example.newwriters.api.BookApi
+import com.example.newwriters.api.MyApiRequest
+import com.example.newwriters.api.ReviewApi
+import com.example.newwriters.api.ServiceBuilder
+import com.example.newwriters.response.ReviewResponse
+import com.example.newwriters.ui.model.Review
+
+class ReviewRepository:MyApiRequest() {
+    val myapi= ServiceBuilder.buildServices(ReviewApi::class.java)
+
+    suspend fun addReiew(review:Review):ReviewResponse{
+        return apiRequest{
+            myapi.addReview(review)
+        }
+    }
+}
