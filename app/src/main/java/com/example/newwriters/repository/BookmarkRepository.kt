98f0090ef499b6b63766cdc65eded2d66581d246
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-16 12:36:03 +0545

    done with bookmarkrepository to post bookmark data

---
+++ app/src/main/java/com/example/newwriters/repository/BookmarkRepository.kt
@@ -0,0 +1,18 @@
+package com.example.newwriters.repository
+
+import com.example.newwriters.api.BookApi
+import com.example.newwriters.api.BookMarkApi
+import com.example.newwriters.api.MyApiRequest
+import com.example.newwriters.api.ServiceBuilder
+import com.example.newwriters.response.BookmarkResponse
+import com.example.newwriters.ui.model.Bookmark
+
+class BookmarkRepository:MyApiRequest() {
+    val myapi= ServiceBuilder.buildServices(BookMarkApi::class.java)
+
+    suspend fun BookmarkBook(bookmark: Bookmark):BookmarkResponse{
+        return apiRequest {
+            myapi.BookmarkBook(ServiceBuilder.token!!,bookmark)
+        }
+    }
+}
