fa2a33d40460a22cb3f644951a32b21275859fba
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-19 13:59:11 +0545

    notification class

---
+++ app/src/main/java/com/example/newwriters/ui/utils/notif.kt
@@ -0,0 +1,31 @@
+package com.example.newwriters.ui.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class notif(val context: Context) {
+    val CHANNEL_1: String = "Channel1"
+    val CHANNEL_2: String = "Channel2"
+
+    // @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel1 =
+                NotificationChannel(CHANNEL_1, "Channel 1", NotificationManager.IMPORTANCE_HIGH)
+            channel1.description = "This is channel 1"
+
+
+            val channel2 =
+                NotificationChannel(CHANNEL_2, "Channel 2", NotificationManager.IMPORTANCE_LOW)
+            channel2.description = "This is channel 2"
+
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel2)
+            notificationManager.createNotificationChannel(channel1)
+        }
+    }
+}
