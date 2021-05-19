bebf5d903ec2f1f8301d438dcef9d848acb5fce9
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-19 14:19:38 +0545

    notification repository

---
+++ app/src/main/java/com/example/newwriters/repository/NotificationRepository.kt
@@ -0,0 +1,22 @@
+package com.example.newwriters.repository
+
+import com.example.newwriters.api.BookMarkApi
+import com.example.newwriters.api.MyApiRequest
+import com.example.newwriters.api.NoticationApi
+import com.example.newwriters.api.ServiceBuilder
+import com.example.newwriters.response.NoticationResponse
+import com.example.newwriters.ui.model.Notification
+
+class NotificationRepository:MyApiRequest() {
+    val myapi= ServiceBuilder.buildServices(NoticationApi::class.java)
+    suspend fun addNotification(notification: Notification):NoticationResponse{
+        return apiRequest {
+            myapi.addNotification(ServiceBuilder.token!!,notification)
+        }
+    }
+    suspend fun getAllNotif():NoticationResponse{
+        return apiRequest {
+            myapi.getallNotification(ServiceBuilder.token!!)
+        }
+    }
+}
