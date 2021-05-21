e1181f9b1ef37029c470af9dca53044a6b0db4ce
gopa11313 <gopalaa9819525993@gmail.com>
2021-05-21 23:57:35 +0545

    bougth books model

---
+++ app/src/main/java/com/example/newwriters/ui/model/BougthBooks.kt
@@ -0,0 +1,10 @@
+package com.example.newwriters.ui.model
+
+import java.util.*
+
+data class BougthBooks(
+    val _id:String?=null,
+    val bookId:String?=null,
+    val date: String?=null,
+) {
+}
